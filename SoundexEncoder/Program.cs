5015487227305ddc1e26ff774660f817a53a40b5
Kristian <kic.me.plz@gmail.com>
2018-03-03 02:19:05 +0000

    Soundex Encoder program

---
+++ SoundexEncoder/Program.cs
@@ -0,0 +1,75 @@
+﻿// Author: Kristian Curcic
+// SID: 14103017
+// CMP5216 Advanced Software Development UG2 2015/16 Assignment 1
+// Exercise 1 - Soundex Encoding
+// Main program
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundexEncoding
+{
+    class SoundexEncodeTest
+    {
+
+        // Main
+        static void Main(string[] args)
+        {
+            SoundexEncodeTest Program = new SoundexEncodeTest();
+            Program.Run();
+            Pause("\n\nPress any key to quit.");
+        }
+
+       // To exit program
+        static void Pause(string s)
+        {
+            Console.Write(s);
+            Console.Read();
+        }
+
+        // All functionality here
+        public void Run()
+        {
+            // Variables
+            string word1, word2;
+            Soundex encode;
+
+            // Short explanation of the program
+            Console.WriteLine("Exercise 1 - Soundex Encoder");
+            Console.WriteLine("Developed by Kristian Curcic");
+            Console.WriteLine("\nThis program encodes words entered by the user to Soundex.");
+            Console.WriteLine("Words can be encoded both partially and fully.");
+            Console.WriteLine("Similar sounding words should be encoded to the same numbers in full Soundex.");
+
+            // Getting user inputs
+            Console.Write("\n\nEnter a word: ");
+            word1 = Console.ReadLine();
+            Console.Write("\nEnter a similar sounding word: ");
+            word2 = Console.ReadLine();
+
+            // Initialise Soundex class
+            encode = new Soundex();
+
+            // Method abbreviations
+            string par1 = encode.EncodeSoundex(word1);
+            string par2 = encode.EncodeSoundex(word2);
+            string ful1 = encode.EncodeSoundexFull(word1);
+            string ful2 = encode.EncodeSoundexFull(word2);
+
+            // Returns partial Soundex encoding 
+            Console.WriteLine("\n{0} is encoded to {1} in Soundex.", word1, par1);
+            Console.WriteLine("{0} is encoded to {1}.", word2, par2);
+
+            // Returns full Soundex encoding
+            Console.WriteLine("\nFully encoded the words are {0} and {1}.", ful1, ful2);
+
+            // Returns whether or not words sound similar
+            if (ful1 == ful2)
+                Console.WriteLine("The words sound similar.");
+            else Console.WriteLine("The words do not sound similar.");
+        }
+    }
+}
---
+++ SoundexEncoder/Soundex.cs
@@ -0,0 +1,106 @@
+﻿// Author: Kristian Curcic
+// SID: 14103017
+// CMP5216 Advanced Software Development UG2 2015/16 Assignment 1
+// Exercise 1 - Soundex Encoding
+// Soundex encoder class
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundexEncoding
+{
+    class Soundex
+    {
+        // Partial encoding
+        public string EncodeSoundex(string text)
+        {
+            text = text.ToUpper(); 
+            // Initialise string builder
+            StringBuilder soundex = new StringBuilder();
+
+            // Loop, looks for characters and adds them to string builder
+            foreach (char chr in text)
+            {
+                if (char.IsLetter(chr)) 
+                    AddChrs(soundex, chr);
+            }
+
+            // Converts StringBuilder to new string and returns it
+            return soundex.ToString();
+        }
+
+        // Full encoding
+        public string EncodeSoundexFull(string text)
+        {
+            text = text.ToUpper();
+            StringBuilder soundex = new StringBuilder();
+
+            foreach (char chr in text)
+            {
+                if (char.IsLetter(chr))
+                    AddChrsFull(soundex, chr);
+            }
+
+            RemoveZeros(soundex);
+            return soundex.ToString();
+        }
+
+        // For partial encoding
+        private void AddChrs(StringBuilder soundex, char chr)
+        {
+            string encoding = GetGroups(chr);
+            // Adds characters to soundex
+            soundex.Append(encoding);
+        }
+
+        // For full encoding
+        private void AddChrsFull(StringBuilder soundex, char chr)
+        {
+            string encoding = GetGroups(chr);
+
+            if (soundex.Length == 0)
+               soundex.Append(encoding);
+
+            else
+            {
+                RemoveZeros(soundex);
+                // Only adds character to soundex if it is different to previous character
+                if (encoding != soundex[soundex.Length - 1].ToString())
+                    soundex.Append(encoding);
+            }
+        }
+
+        // Encodes letter groups in the string to numbers
+        private string GetGroups(char chr)
+        {
+            string CharString = chr.ToString();
+
+            if ("AEIOUHWY".Contains(CharString))
+                return "0";
+            else if ("BFPV".Contains(CharString))
+                return "1";
+            else if ("CGJKQSXZ".Contains(CharString))
+                return "2";
+            else if ("DT".Contains(CharString))
+                return "3";
+            else if (chr == 'L')
+                return "4";
+            else if ("MN".Contains(CharString))
+                return "5";
+            else if (chr == 'R')
+                return "6";
+            // In case anything other than a letter is typed in
+            else return "";
+        }
+
+        // Removes zeros for full encoding
+        private void RemoveZeros(StringBuilder soundex)
+        {
+            soundex.Replace("0", "");
+        }
+
+    }
+}
